d27bd60e8d8e98bd4d3c5d7f22947e5255e71667
Daniel Dantas Marotti <dantasmaarotti@gmail.com>
2025-03-05 12:16:25 -0300

    feat: Add favicon and change title

--- constants.ts
+++ constants.ts
@@ -1,5 +1,6 @@
 export default {
     platform_name: "Audivino",
+    platform_description: "Palavra divina todo dia",
     contact: {
         mail: "audivino@catap.io",
         phone: "(11) 99999-9999",
